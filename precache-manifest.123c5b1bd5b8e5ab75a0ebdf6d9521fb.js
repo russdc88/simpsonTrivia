94b4fa88faa2bcc0318227aba50db84059bf564a
russdc88 <russdc88@gmail.com>
2019-01-02 11:27:25 -0700

    Updates

---
+++ precache-manifest.123c5b1bd5b8e5ab75a0ebdf6d9521fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simpsonTrivia/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "37c29cfe9c81a02edd2f",
+    "url": "/simpsonTrivia/static/js/runtime~main.37c29cfe.js"
+  },
+  {
+    "revision": "d1756804508688faede4",
+    "url": "/simpsonTrivia/static/js/main.d1756804.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/simpsonTrivia/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "d1756804508688faede4",
+    "url": "/simpsonTrivia/static/css/main.9ef1a5bc.chunk.css"
+  },
+  {
+    "revision": "fc51a4eada092be20e57fbbf199a50f6",
+    "url": "/simpsonTrivia/index.html"
+  }
+];
